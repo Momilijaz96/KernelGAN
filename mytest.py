ea76b2928d7064de9f3e5fb77599cf8bf2593f23
Momilijaz96 <momilijaz@gmail.com>
2022-04-22 22:29:29 -0400

    added conf

--- mytest.py
+++ mytest.py
@@ -1,7 +1,9 @@
 import torch
 from networks import Generator
+from configs import Config
 
-model = Generator()
+conf = Config().parse()
+model = Generator(conf)
 inp=torch.randn((1, 3, 128 , 128 ,4))
 op=model(inp)
 print("Op shape: ",op.shape)
