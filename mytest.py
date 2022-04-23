8ce64b2f3f8a1f4f7a01c8f684eb71b135334f6d
Momal Ijaz <53872365+Momilijaz96@users.noreply.github.com>
2022-04-23 13:35:59 -0400

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -4,14 +4,13 @@ from kernelGAN import KernelGAN
 from learner import Learner
 import tqdm
 
-def train(conf):
-    gan = KernelGAN(conf)
-    learner = Learner()
+def test(conf):
+   
     data = DataGenerator(conf, gan)
     for iteration in tqdm.tqdm(range(conf.max_iters), ncols=60):
         [g_in, d_in] = data.__getitem__(iteration)
         print("Generator input: ",g_in.shape)
         print("Discriminator input: ",d_in.shape)
-
+        break
 conf=Config().parse()
 train(conf)
