165b125d4eb7f75a697c9f9c5d162c1c0ae8f3fb
Momal Ijaz <53872365+Momilijaz96@users.noreply.github.com>
2022-04-23 17:46:52 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import numpy as np
 from torch.utils.data import Dataset
 from imresize import imresize
-from util import read_video, read_image, create_gradient_map, im2tensor, create_probability_map, nn_interpolation
+from util import read_video, read_image, create_gradient_map_video, im2tensor, create_probability_map, nn_interpolation
 import torch
 
 class DataGenerator(Dataset):
@@ -69,8 +69,8 @@ class DataGenerator(Dataset):
         # Create loss maps for input image and downscaled one
         #loss_map_big = create_gradient_map(self.input_image)
         #loss_map_sml = create_gradient_map(imresize(im=self.input_image, scale_factor=scale_factor, kernel='cubic'))
-        loss_map_big = create_gradient_map(self.input_video)
-        loss_map_sml = create_gradient_map(imresize(im=self.input_video, scale_factor=scale_factor, kernel='cubic'))
+        loss_map_big = create_gradient_map_video(self.input_video)
+        loss_map_sml = create_gradient_map_video(imresize(im=self.input_video, scale_factor=scale_factor, kernel='cubic'))
         
         # Create corresponding probability maps
         prob_map_big = create_probability_map(loss_map_big, self.d_input_shape)
