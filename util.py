19e5b8f2d388c4416a0a0c79291a766cacf89c3f
Momal Ijaz <53872365+Momilijaz96@users.noreply.github.com>
2022-04-23 17:33:13 -0400

    loss maps for videos

--- util.py
+++ util.py
@@ -107,6 +107,26 @@ def create_gradient_map(im, window=5, percent=.97):
     # Normalizing: sum of map = numel
     return loss_map / np.mean(loss_map)
 
+def create_gradient_map_video(vid, window=5, percent=.97):
+    """Create a gradient map of the video blurred with a rect of size window and clips extreme values"""
+    loss_maps = []
+    for im in vid:
+        # Calculate gradients
+        gx, gy = np.gradient(rgb2gray(im))
+        # Calculate gradient magnitude
+        gmag, gx, gy = np.sqrt(gx ** 2 + gy ** 2), np.abs(gx), np.abs(gy)
+        # Pad edges to avoid artifacts in the edge of the image
+        gx_pad, gy_pad, gmag = pad_edges(gx, int(window)), pad_edges(gy, int(window)), pad_edges(gmag, int(window))
+        lm_x, lm_y, lm_gmag = clip_extreme(gx_pad, percent), clip_extreme(gy_pad, percent), clip_extreme(gmag, percent)
+        # Sum both gradient maps
+        grads_comb = lm_x / lm_x.sum() + lm_y / lm_y.sum() + gmag / gmag.sum()
+        # Blur the gradients and normalize to original values
+        loss_map = convolve2d(grads_comb, np.ones(shape=(window, window)), 'same') / (window ** 2)
+        # Normalizing: sum of map = numel
+        loss_map = loss_map / np.mean(loss_map)
+        #Loss maps gathered for each frame in a list
+        loss_maps.append(loss_map)
+    return loss_maps
 
 def create_probability_map(loss_map, crop):
     """Create a vector of probabilities corresponding to the loss map"""
