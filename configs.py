284c2edc6fd3f775129e8c42479d093ef7c1ee3b
Momal Ijaz <53872365+Momilijaz96@users.noreply.github.com>
2022-04-23 13:32:00 -0400

    updated line61 clean_fle_name function

--- configs.py
+++ configs.py
@@ -58,7 +58,9 @@ class Config:
 
     def clean_file_name(self):
         """Retrieves the clean image file_name for saving purposes"""
-        self.conf.img_name = self.conf.input_image_path.split('/')[-1].replace('ZSSR', '') \
+        #self.conf.img_name = self.conf.input_image_path.split('/')[-1].replace('ZSSR', '') \
+        #    .replace('real', '').replace('__', '').split('_.')[0].split('.')[0]
+        self.conf.img_name = self.conf.input_video_path.split('/')[-1].replace('ZSSR', '') \
             .replace('real', '').replace('__', '').split('_.')[0].split('.')[0]
 
     def set_gpu_device(self):
